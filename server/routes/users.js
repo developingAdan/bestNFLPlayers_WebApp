7a2522ad46532a22278eaa62286fc718b213bb16
Adan Garcia <developingadan@gmail.com>
2023-05-21 15:53:49 -0500

    deployment

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,7 +2,7 @@
 
 import express from 'express';
 import jwt from 'jsonwebtoken';
-import bcrpyt from 'bcrypt';
+import bcrypt from 'bcrypt';
 import { UserModel } from "../models/Users.js"
 
 const router = express.Router()
@@ -63,49 +63,49 @@ router.get("/", async (req, res) => {
 
 
 router.post("/register", async (req, res) => {
-    try {
-      const { username, password } = req.body;
-  
-      const user = await UserModel.findOne({ username: username });
-  
-      if (user) {
-        return res.json({ message: "This user already exists" });
-      }
-  
-      const hashedPassword = await bcrypt.hash(password, 10);
-  
-      const newUser = new UserModel({ username, password: hashedPassword });
-      await newUser.save();
-      res.json({ message: "User registered successfully" });
-    } catch (error) {
-      console.error("Error in register:", error);
-      res.status(500).json({ message: "Internal server error" });
+  try {
+    const { username, password } = req.body;
+
+    const user = await UserModel.findOne({ username: username });
+
+    if (user) {
+      return res.json({ message: "This user already exists" });
     }
-  });
-  
-  router.post("/login", async (req, res) => {
-    try {
-      const { username, password } = req.body;
-      const user = await UserModel.findOne({ username });
-  
-      if (!user) {
-        return res.status(400).json({ message: "User doesn't exist" });
-      }
-  
-      const isPasswordValid = await bcrypt.compare(password, user.password);
-  
-      if (!isPasswordValid) {
-        return res.status(400).json({ message: "Username or password is incorrect" });
-      }
-  
-      const token = jwt.sign({ id: user._id }, "secret");
-      res.json({ token, userID: user._id });
-    } catch (error) {
-      console.error("Error in login:", error);
-      res.status(500).json({ message: "Internal server error" });
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = new UserModel({ username, password: hashedPassword });
+    await newUser.save();
+    res.json({ message: "User registered successfully" });
+  } catch (error) {
+    console.error("Error in register:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await UserModel.findOne({ username });
+
+    if (!user) {
+      return res.status(400).json({ message: "User doesn't exist" });
     }
-  });
-  
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(400).json({ message: "Username or password is incorrect" });
+    }
+
+    const token = jwt.sign({ id: user._id }, "secret");
+    res.json({ token, userID: user._id });
+  } catch (error) {
+    console.error("Error in login:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
 
 export { router as userRouter } // why export this as object? (Ask ChatGPT)
 
